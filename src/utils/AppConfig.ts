e341fd6175d909e09913de539a431cb218f2ee82
nawa316 <muhammadadedzakwan@gmail.com>
2026-06-14 17:39:04 +0700

    fix: error build (last)

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,9 +1,13 @@
+import type { LocalePrefixMode } from 'next-intl/routing';
+
+const localePrefix: LocalePrefixMode = 'never';
+
 // FIXME: Update this configuration file based on your project information
 export const AppConfig = {
   name: 'Halaloka',
   i18n: {
     locales: ['en'],
     defaultLocale: 'en',
-    localePrefix: 'never',
+    localePrefix,
   },
 };
